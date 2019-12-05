d5ec25178e456c187b0e4f589d721108e5f13ea9
Victor Opere <victoropere@gmail.com>
2019-12-05 22:37:00 +0300

    just done with creating the admin part

--- listings/admin.py
+++ listings/admin.py
@@ -4,5 +4,17 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_published','price','list_date','realtor')
+    list_display_links = ('id' , 'title')
+    list_filter = ('realtor', 'price')
+    list_editable = ('is_published',)
+    search_fields = ('title','price','address','city')
+    list_per_page = 25
+    
+    
+
+admin.site.register(Listing,ListingAdmin)
 
